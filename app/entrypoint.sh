16afe366825e243b0a7382f249c9e46b475857fb
Jeremie Bidaux <jeremie.bidaux@gmail.com>
2024-10-31 16:11:08 +0100

    setting up django

---
+++ app/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+	echo "Waiting for postgres.."
+
+	while ! nc -z $SQL_HOST $SQL_PORT; do
+		sleep 0.1
+	done
+
+	echo "PostgreSQL started"
+fi
+
+#remove all data from database
+python manage.py flush --no-input
+#applies all database migrations -> setting up the database schema to match current state of Django models.
+python manage.py migrate
+
+exec "$@"
